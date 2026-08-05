b43687a2142445a033b69cf8314af8730a07ca8c
Ayush Jha <ayjha2006@gmail.com>
2026-08-05 19:19:12 +0530

    patterns_codes: added pattern_5v2.cpp

---
+++ patterns_codes/pattern_5v2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void print_5 (int x) {
+    for (int i=x; i>0; i--){
+        for (int j=0; j<i; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+
+int main () {
+    int n;
+    cout<<"Enter n for pattern: ";
+    cin>>n;
+
+    print_5(n);
+
+    return 0;
+}
