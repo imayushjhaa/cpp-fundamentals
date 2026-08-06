c8dc561f73b022075a51766c2ea03f4399b53944
Ayush Jha <ayjha2006@gmail.com>
2026-08-06 21:38:24 +0530

    patterns_codes: added comments in pattern_7.cpp

--- patterns_codes/pattern_7.cpp
+++ patterns_codes/pattern_7.cpp
@@ -2,13 +2,19 @@
 using namespace std;
 
 void print_7 (int x) {
-    for(int i=0; i<x; i++){
+    for(int i=0; i<x; i++){     // outer loop
+
+        //for printing spaces
         for(int j=0; j<x-i-1; j++){
             cout<<" ";
         }
+
+        //for printing stars
         for(int j=0; j<2*i+1; j++){
             cout<<"*";
         }
+
+        //for printing spaces
         for(int j=0; j<x-i-1; j++){
             cout<<" ";
         }
